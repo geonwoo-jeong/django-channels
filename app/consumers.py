192ee241585c3421c7d08c67b0c2d9300812055d
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2022-12-29 15:54:38 +0900

    object data

--- app/consumers.py
+++ app/consumers.py
@@ -1,6 +1,16 @@
 from channels.generic.websocket import WebsocketConsumer
+import json
 
 class EchoConsumer(WebsocketConsumer):
     
     def receive(self, text_data=None, bytes_data=None):
-        self.send(f'You said: {text_data}')
+        print(text_data)
+        obj = json.loads(text_data)
+        print('received :', obj)
+
+        json_string = json.dumps({
+            "content": obj["content"],
+            "user": obj["user"],
+        })
+
+        self.send(json_string)
